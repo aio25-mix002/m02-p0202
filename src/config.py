ad838c88cf9307bd14382cb13ef60f4ec9ca5178
Hung Doan <hung.doantan@gmail.com>
2025-08-02 01:05:59 +0700

    Fix FAISS issue and add config file

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import os
+import random
+from pathlib import Path
+import numpy as np
+
+
+class AppConfig:
+    SEED = 42
+    TEST_SIZE = 0.2
+
+    class Path:
+        APP_HOME = Path(os.getenv("APP_HOME", Path(__file__).parent.parent))
+        RAW_DATA_DIR = APP_HOME / "data"
+        RAW_DATA_FILE = RAW_DATA_DIR / "spam.csv"
+
+
+def seed_everything(seed: int = AppConfig.SEED):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
